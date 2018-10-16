5cec5febaab9fbc59873337c854e9d6d6949ac7e
benwilcock <benwilcock@gmail.com>
2018-10-16 21:35:18 +0200

    pointed the UI at the TraderApp

--- trader-app-ui/src/utils/config.js
+++ trader-app-ui/src/utils/config.js
@@ -1,7 +1,7 @@
 export const HostURLsMapping = {
   'localhost': 'https://esrefarch-demo-trader-app.cfapps.io',
-  'esrefarch-demo-trader-ui.cfapps.io': 'https://esrefarch-demo-trader-app.cfapps.io',
-  'axontrader.cfapps.io': 'https://esrefarch-demo-trader-app.cfapps.io',
+  'esrefarch-demo-trader-ui-reflective-roan.cfapps.io': 'https://esrefarch-demo-trader-app-shiny-oryx.cfapps.io',
+  'devoxxtrader.cfapps.io': 'https://esrefarch-demo-trader-app-shiny-oryx.cfapps.io',
 };
 export const ApiConfig = () => {
   const hostname = window.location.hostname;
